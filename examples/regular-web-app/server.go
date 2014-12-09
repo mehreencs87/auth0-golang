eb18a29aed660cfd344493da5e288021abf509c3
Martin Gontovnikas <martin@gon.to>
2014-12-09 14:48:13 -0300

    Added isAuthenticated middleware

--- examples/regular-web-app/server.go
+++ examples/regular-web-app/server.go
@@ -3,7 +3,9 @@ package main
 import (
 	"github.com/auth0/auth0-go/examples/regular-web-app/routes/callback"
 	"github.com/auth0/auth0-go/examples/regular-web-app/routes/home"
+	"github.com/auth0/auth0-go/examples/regular-web-app/routes/middlewares"
 	"github.com/auth0/auth0-go/examples/regular-web-app/routes/user"
+	"github.com/codegangsta/negroni"
 	"github.com/gorilla/mux"
 	"net/http"
 )
@@ -13,7 +15,10 @@ func StartServer() {
 
 	r.HandleFunc("/", home.HomeHandler)
 	r.HandleFunc("/callback", callback.CallbackHandler)
-	r.HandleFunc("/user", user.UserHandler)
+	r.Handle("/user", negroni.New(
+		negroni.HandlerFunc(middlewares.IsAuthenticated),
+		negroni.Wrap(http.HandlerFunc(user.UserHandler)),
+	))
 	r.PathPrefix("/public/").Handler(http.StripPrefix("/public/", http.FileServer(http.Dir("public/"))))
 	http.Handle("/", r)
 	http.ListenAndServe(":3000", nil)
