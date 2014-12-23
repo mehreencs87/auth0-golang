60172d4a2b9e2d87c1004b4b125bfdce241eda4f
Martin Gontovnikas <martin@gon.to>
2014-12-23 12:54:08 -0300

    Tab changes

--- examples/regular-web-app/routes/callback/callback.go
+++ examples/regular-web-app/routes/callback/callback.go
@@ -3,7 +3,7 @@ package callback
 import (
 	_ "crypto/sha512"
 	"encoding/json"
-        "github.com/auth0/auth0-golang/examples/regular-web-app/app"
+	"github.com/auth0/auth0-golang/examples/regular-web-app/app"
 	"golang.org/x/oauth2"
 	"io/ioutil"
 	"net/http"
@@ -14,27 +14,27 @@ func CallbackHandler(w http.ResponseWriter, r *http.Request) {
 
 	domain := os.Getenv("AUTH0_DOMAIN")
 
-        conf := &oauth2.Config{
-                ClientID:     os.Getenv("AUTH0_CLIENT_ID"),
-                ClientSecret: os.Getenv("AUTH0_CLIENT_SECRET"),
-                RedirectURL:  os.Getenv("AUTH0_CALLBACK_URL"),
-                Scopes:       []string{"openid", "profile"},
-                Endpoint: oauth2.Endpoint{
-                        AuthURL:  "https://" + domain + "/authorize",
-                        TokenURL: "https://" + domain + "/oauth/token",
-                },
+	conf := &oauth2.Config{
+		ClientID:     os.Getenv("AUTH0_CLIENT_ID"),
+		ClientSecret: os.Getenv("AUTH0_CLIENT_SECRET"),
+		RedirectURL:  os.Getenv("AUTH0_CALLBACK_URL"),
+		Scopes:       []string{"openid", "profile"},
+		Endpoint: oauth2.Endpoint{
+			AuthURL:  "https://" + domain + "/authorize",
+			TokenURL: "https://" + domain + "/oauth/token",
+		},
 	}
 
 	code := r.URL.Query().Get("code")
 
-        token, err := conf.Exchange(oauth2.NoContext, code)
+	token, err := conf.Exchange(oauth2.NoContext, code)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 
 	// Getting now the userInfo
-        client := conf.Client(oauth2.NoContext, token)
+	client := conf.Client(oauth2.NoContext, token)
 	resp, err := client.Get("https://" + domain + "/userinfo")
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
